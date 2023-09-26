411978f9bd6cad4134eb066e00917689cc394c40
soniashankar2022n <soniashankar2022@gmail.com>
2023-09-26 10:30:39 -0600

    adding modified calculator prgm

--- IdeaProjects/java_august/src/september16/CalculatorPrgm.java
+++ IdeaProjects/java_august/src/september16/CalculatorPrgm.java
@@ -3,8 +3,8 @@ package september16;
 public class CalculatorPrgm {
 
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 5;
+        double num1 = 10.0;
+        double num2 = 5.0;
         char operator = '*';
 
         double result;
