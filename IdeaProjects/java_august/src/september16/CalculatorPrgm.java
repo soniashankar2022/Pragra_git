084aa7849fe5e450574727e74c27a55f22bd104e
soniashankar2022n <soniashankar2022@gmail.com>
2023-09-26 11:33:45 -0600

    adding modified calculator prgm

--- IdeaProjects/java_august/src/september16/CalculatorPrgm.java
+++ IdeaProjects/java_august/src/september16/CalculatorPrgm.java
@@ -9,6 +9,13 @@ public class CalculatorPrgm {
 
         double result;
 
+        System.out.println("Welcome to my calculator");
+        System.out.println("1. press + for addition");
+        System.out.println("2. press - for Subtraction");
+        System.out.println("3. Press * fro multiplication");
+        System.out.println("4 .press / for division");
+
+
         if (operator == '+') {
             result = num1 + num2;
             System.out.println("Additon of two nuumber is : " + result);
--- IdeaProjects/java_august/src/september16/ScannerDemo.java
+++ IdeaProjects/java_august/src/september16/ScannerDemo.java
@@ -1,19 +1,29 @@
 package september16;
 
+import java.util.Scanner;
+
 public class ScannerDemo {
 
     public static void main(String[] args) {
-        double num1 = 10.0;
-        double num2 = 5.0;
-        char operator = '/';
 
-        double result;
         System.out.println("Welcome to my calculator");
         System.out.println("1. press + for addition");
         System.out.println("2. press - for Subtraction");
         System.out.println("3. Press * fro multiplication");
         System.out.println("4 .press / for division");
 
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the operation to perform");
+        char operator = scanner.next().charAt(0);
+        System.out.println("Enter the first number for calculation");
+        int num1 = scanner.nextInt();
+        System.out.println("enter the second number for calculation");
+        int num2 = scanner.nextInt();
+
+
+
+        int  result;
         if (operator == '+') {
             result = num1 + num2;
             System.out.println("Additon of two nuumber is : " + result);
