122e25c4aa304b5ebb95e682a79bd75c47fcebe3
soniashankar2022n <soniashankar2022@gmail.com>
2023-09-26 13:48:14 -0600

    adding modified prgm files

--- IdeaProjects/java_august/src/september16/IvrSwitchProgram.java
+++ IdeaProjects/java_august/src/september16/IvrSwitchProgram.java
@@ -6,38 +6,74 @@ public class IvrSwitchProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Press 1 for English");
+        System.out.println("Press 2 for French");
+        System.out.println("Press 3 for Spanish");
+        System.out.println("Press the number for language selection ");
 
-            System.out.println("Welcome to the IVR system!");
-            System.out.println("Please select an option:");
-            System.out.println("1. Press 1 for account information");
-            System.out.println("2. Press 2 for billing information");
-            System.out.println("3. Press 3 to speak to a representative");
-            System.out.println("4. Press 4 to exit");
+        int languageChoice = scanner.nextInt();
+        scanner.nextLine();
 
-            int choice = scanner.nextInt();
+        if (languageChoice == 1) {
+            System.out.println("You selected English.");
+            System.out.println("Press 1 for Internet");
+            System.out.println("Press 2 for Mobile");
+            System.out.println("Press 3 for Cable");
+
+            int serviceChoice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice) {
-                case 1:
-                    System.out.println("You selected account information.");
-
-                    break;
-                case 2:
-                    System.out.println("You selected billing information.");
-
-                    break;
-                case 3:
-                    System.out.println("Please wait while we connect you to a representative.");
-
-                    break;
-                case 4:
-                    System.out.println("Thank you for using our IVR system. Goodbye!");
-
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+            if (serviceChoice == 1) {
+                System.out.println("New connection established for Internet.");
+            } else if (serviceChoice == 2) {
+                System.out.println("Existing connection for Mobile.");
+            } else if (serviceChoice == 3) {
+                System.out.println("Bill payment for Cable.");
+            } else {
+                System.out.println("Invalid service choice.");
             }
+
+        } else if (languageChoice == 2) {
+            System.out.println("You selected French.");
+            System.out.println("Press 1 for Internet");
+            System.out.println("Press 2 for Mobile");
+            System.out.println("Press 3 for Cable");
+
+            int serviceChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (serviceChoice == 1) {
+                System.out.println("New connection established for Internet.");
+            } else if (serviceChoice == 2) {
+                System.out.println("Existing connection for Mobile.");
+            } else if (serviceChoice == 3) {
+                System.out.println("Bill payment for Cable.");
+            } else {
+                System.out.println("Invalid service choice.");
+            }
+
+        } else if (languageChoice == 3) {
+            System.out.println("You selected Spanish.");
+            System.out.println("Press 1 for Internet");
+            System.out.println("Press 2 for Mobile");
+            System.out.println("Press 3 for Cable");
+
+            int serviceChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (serviceChoice == 1) {
+                System.out.println("New connection established for Internet.");
+            } else if (serviceChoice == 2) {
+                System.out.println("Existing connection for Mobile.");
+            } else if (serviceChoice == 3) {
+                System.out.println("Bill payment for Cable.");
+            } else {
+                System.out.println("Invalid service choice.");
+            }
+
         }
     }
+}
 
 
 
