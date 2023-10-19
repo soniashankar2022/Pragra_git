6380894e6156eec1a31223eabc50b9f1ba3afe24
soniashankar2022 <soniashankar2022@gmail.com>
2023-10-19 12:48:27 -0600

    adding  Programs

--- IdeaProjects/java_august/src/DailyPractise/String/Stringconcat.java
+++ IdeaProjects/java_august/src/DailyPractise/String/Stringconcat.java
@@ -8,3 +8,5 @@ public class Stringconcat {
     }
 }
 //this is because string is immutable .so once string cretead the object with reference name sonia ..whatever action we are trying to do it wont allow to perform.
+//if we are trying to perform any changes with those changes the new object will be ceated .
+//we are not assigning the new object for any varaible so it is eligible for garbage collection
