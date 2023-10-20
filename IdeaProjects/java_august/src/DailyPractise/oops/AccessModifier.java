b46448777e3d92a7bf0de28fc67c23938113778c
soniashankar2022 <soniashankar2022@gmail.com>
2023-10-20 11:23:37 -0600

    adding  Programs

---
+++ IdeaProjects/java_august/src/DailyPractise/oops/AccessModifier.java
@@ -0,0 +1,27 @@
+package DailyPractise.oops;
+
+public class AccessModifier {
+
+        private String name;
+
+        // getter method
+        public String getName() {
+            return this.name;
+        }
+        // setter method
+        public void setName(String name) {
+            this.name= name;
+        }
+
+
+        public static void main(String[] main){
+            AccessModifier d = new AccessModifier();
+
+            // access the private variable using the getter and setter
+            d.setName("Programiz");
+            d.getName();
+            System.out.println(d.getName());
+          
+        }
+    }
+
---
+++ IdeaProjects/java_august/src/DailyPractise/oops/Recursion.java
@@ -0,0 +1,17 @@
+package DailyPractise.oops;
+
+public class Recursion {
+
+    public static void main(String[]args){
+        sayhi(5);
+    }
+    static void sayhi(int n){
+        if(n==0){
+            System.out.println("stop");
+        }else {
+            System.out.println("HI");
+            n--;
+            sayhi(n);
+        }
+    }
+}
