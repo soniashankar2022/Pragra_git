c20f74c1d7a46dc81e0215cf26427e6068cee9f6
soniashankar2022 <soniashankar2022@gmail.com>
2023-10-23 13:05:24 -0600

    adding Assignment Program

---
+++ IdeaProjects/java_august/src/Assignments1/NaturalNumbers.java
@@ -0,0 +1,16 @@
+package Assignments1;
+
+
+//Write a Java program to display the first 10 natural numbers.
+
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        int a=10;
+        for(int i=1;i<=a;i++){
+
+            System.out.println( i);
+        }
+
+    }
+
+}
