502806fcfd87e32ec3a0dc8d2713da7095b393f7
soniashankar2022 <soniashankar2022@gmail.com>
2023-10-24 14:57:24 -0600

    adding Assignment Program

---
+++ IdeaProjects/java_august/src/AssignmentPragra/OCT17Assignment/ReverseString.java
@@ -0,0 +1,22 @@
+package AssignmentPragra.OCT17Assignment;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str="I am computer programmer";
+
+        //we are now converting the string into string array
+        String a[]=str.split(" ");
+        for(int i=0;i<a.length;i++){
+
+            System.out.print(a[i] + " ");
+
+        }
+        //reverse format
+        System.out.println(" ");
+        for(int i=a.length-1;i>=0;i--){
+            System.out.print(a[i] + " " );
+        }
+    }
+
+
+}
