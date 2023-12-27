b26d2ff923cc24aa4506bf2be4bc3593d4a146e7
soniashankar2022 <soniashankar2022@gmail.com>
2023-12-27 10:59:09 -0700

    Merge coding

--- IdeaProjects/SeliniumPractise/src/main/java/GreenKartPractice/AmazonQuestionGreenkart.java
+++ IdeaProjects/SeliniumPractise/src/main/java/GreenKartPractice/AmazonQuestionGreenkart.java
@@ -7,18 +7,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class AmazonQuestionGreenkart {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-        String[] veggies = {"Cucumber", "Brocolli", "Cauliflower","Raspberry"};
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        String[] veggies = {"Cucumber", "Brocolli", "Cauliflower", "Raspberry"};
+        itemsaddes(driver, veggies);
+        driver.findElement(By.xpath("//img[@alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+        driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
+        System.out.println(driver.findElement(By.xpath("//span[@class='promoInfo']")).getText());
+        driver.quit();
+    }
+
+    public static void itemsaddes(WebDriver driver, String[] veggies) throws InterruptedException {
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-        Thread.sleep(4000);
+        //Thread.sleep(4000);
         for (int i = 0; i < products.size(); i++) {
             String name = products.get(i).getText();
             String[] spilt = name.split("-");
@@ -35,10 +48,12 @@ public class AmazonQuestionGreenkart {
                     break;
                 }
 
+
             }
 
 
         }
     }
 }
+
 
